8f3f3cc1a40150021aeac4014d3c3f7c02dec509
Yikuang_Luo <Yikuang_Luo@on-bright.com>
2018-09-27 15:17:05 +0800

    modify the key in add local scene

--- springcloud-consumer-feign-config-swagger/src/main/java/com/bright/apollo/controller/FacadeController.java
+++ springcloud-consumer-feign-config-swagger/src/main/java/com/bright/apollo/controller/FacadeController.java
@@ -772,8 +772,8 @@ public class FacadeController extends BaseController {
 				oboxSceneNumber = Integer.parseInt(data.substring(6, 8), 16);
 				while (System.currentTimeMillis() - startTime < max_waitting_time) {
 					try {
-						logger.info("===key:"+sceneName+oboxSerialId+sceneGroup+oboxSceneNumber);
-						reply = cmdCache.getLocalSceneInfo(sceneName, oboxSerialId, sceneGroup, oboxSceneNumber);
+						logger.info("===key:"+sceneName.trim()+oboxSerialId+sceneGroup+oboxSceneNumber);
+						reply = cmdCache.getLocalSceneInfo(sceneName.trim(), oboxSerialId, sceneGroup, oboxSceneNumber);
 						if (StringUtils.isEmpty(reply)) {
 							TimeUnit.MILLISECONDS.sleep(150);
 						} else {
