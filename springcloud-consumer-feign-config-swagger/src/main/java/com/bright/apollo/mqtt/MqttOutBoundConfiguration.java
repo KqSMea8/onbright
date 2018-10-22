944a1a852f6fa96977503ef1ad866c1945bd0474
lyk <Yikuang_Luo@on-bright.com>
2018-10-22 11:16:43 +0800

    wifiHander upload bug

--- springcloud-consumer-feign-config-swagger/src/main/java/com/bright/apollo/mqtt/MqttOutBoundConfiguration.java
+++ springcloud-consumer-feign-config-swagger/src/main/java/com/bright/apollo/mqtt/MqttOutBoundConfiguration.java
@@ -1,47 +1,47 @@
-package com.bright.apollo.mqtt;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
-import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
-import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandler;
-
-@Configuration
-public class MqttOutBoundConfiguration {
-
-//    @Autowired
-//    private MqttProperties mqttProperties;
-
-    @Bean
-    public MessageChannel outChannel() {
-        return new DirectChannel();
-    }
-
-    @Bean
-    public MqttPahoClientFactory mqttClientFactory(){
-        DefaultMqttPahoClientFactory defaultMqttPahoClientFactory = new DefaultMqttPahoClientFactory();
-        defaultMqttPahoClientFactory.setServerURIs("tcp://localhost.215:1883");
-        defaultMqttPahoClientFactory.setCleanSession(false);
-        defaultMqttPahoClientFactory.setUserName("admin");
-        defaultMqttPahoClientFactory.setPassword("admin");
-        return defaultMqttPahoClientFactory;
-    }
-
-
-    @Bean(name = "mqttPahoMessageHandler")
-    @ServiceActivator(inputChannel = "outChannel")
-    public MessageHandler mqttOutbound(){
-        MqttPahoMessageHandler mqttPahoMessageHandler =
-                new MqttPahoMessageHandler("aliOutbound",mqttClientFactory());
-        mqttPahoMessageHandler.setAsync(true);
-        mqttPahoMessageHandler.setDefaultTopic("topic1");
-        return mqttPahoMessageHandler;
-    }
-
-
-}
+//package com.bright.apollo.mqtt;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.integration.annotation.ServiceActivator;
+//import org.springframework.integration.channel.DirectChannel;
+//import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+//import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+//import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+//import org.springframework.messaging.MessageChannel;
+//import org.springframework.messaging.MessageHandler;
+//
+//@Configuration
+//public class MqttOutBoundConfiguration {
+//
+////    @Autowired
+////    private MqttProperties mqttProperties;
+//
+//    @Bean
+//    public MessageChannel outChannel() {
+//        return new DirectChannel();
+//    }
+//
+//    @Bean
+//    public MqttPahoClientFactory mqttClientFactory(){
+//        DefaultMqttPahoClientFactory defaultMqttPahoClientFactory = new DefaultMqttPahoClientFactory();
+//        defaultMqttPahoClientFactory.setServerURIs("tcp://localhost.215:1883");
+//        defaultMqttPahoClientFactory.setCleanSession(false);
+//        defaultMqttPahoClientFactory.setUserName("admin");
+//        defaultMqttPahoClientFactory.setPassword("admin");
+//        return defaultMqttPahoClientFactory;
+//    }
+//
+//
+//    @Bean(name = "mqttPahoMessageHandler")
+//    @ServiceActivator(inputChannel = "outChannel")
+//    public MessageHandler mqttOutbound(){
+//        MqttPahoMessageHandler mqttPahoMessageHandler =
+//                new MqttPahoMessageHandler("aliOutbound",mqttClientFactory());
+//        mqttPahoMessageHandler.setAsync(true);
+//        mqttPahoMessageHandler.setDefaultTopic("topic1");
+//        return mqttPahoMessageHandler;
+//    }
+//
+//
+//}
