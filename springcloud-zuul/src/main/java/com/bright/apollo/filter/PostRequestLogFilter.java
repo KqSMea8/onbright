2b896a1283443469ea269f36e8d8bcad28169aeb
Yikuang_Luo <Yikuang_Luo@on-bright.com>
2018-10-24 17:30:30 +0800

    add common-io jar

--- springcloud-zuul/src/main/java/com/bright/apollo/filter/PostRequestLogFilter.java
+++ springcloud-zuul/src/main/java/com/bright/apollo/filter/PostRequestLogFilter.java
@@ -2,10 +2,6 @@ package com.bright.apollo.filter;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
