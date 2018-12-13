abb4a89d8b19199761763b3c742d96ac78deaa7d
lyk <Yikuang_Luo@on-bright.com>
2018-12-13 18:24:20 +0800

    ir bug

--- springcloud-ali2/src/main/java/com/bright/apollo/controller/AliDeviceController.java
+++ springcloud-ali2/src/main/java/com/bright/apollo/controller/AliDeviceController.java
@@ -347,6 +347,7 @@ public class AliDeviceController {
 		Map<String,Object> resMap = new HashMap<String,Object>();
 		try {
 			res.setData(getRemoteControlList(brandId,deviceType));
+//			getRemoteControlList2("104","7","");
 			res.setStatus(ResponseEnum.SelectSuccess.getStatus());
 			res.setMessage(ResponseEnum.SelectSuccess.getMsg());
 		} catch (Exception e) {
@@ -355,6 +356,47 @@ public class AliDeviceController {
 			res.setMessage(ResponseEnum.Error.getMsg());
 		}
 		return res;
+	}
+
+	private com.alibaba.fastjson.JSONObject getRemoteControlList2(String brandId,String deviceType,String src) throws Exception {
+		com.alibaba.fastjson.JSONObject resMap = new com.alibaba.fastjson.JSONObject();
+		TYaokonyunDevice yaokonyunDevice = getYaoKongDevice();
+		List<String> strings = new ArrayList<String>();
+		strings.add("bid="+brandId);
+		strings.add("t=7");
+		strings.add("r=1,38000,341,169,24,64,23,22,23,22,23,64,24,21,24,21,24,63,24,21,24,21,24,63,24,21,24,63,24,21,24,21,24,21,24,21,24,21,24,21,24,21,25,21,24,21,24,21,24,21,24,21,24,21,24,21,24,21,24,21,24,63,24,21,24,64,24,21,23,22,23,64,24,21,24");
+		strings.add("zip=1");
+		String result = yaoKongYunSend
+				.postMethod(strings,yaokonyunDevice,yaoKongYunConfig.getUrlPrefix()+"?c=m");
+		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+		MatchRemoteControlResult remoteControlResult = gson.fromJson(result,MatchRemoteControlResult.class);
+
+		if(remoteControlResult==null||remoteControlResult.getSm()==0){
+//            resMap.put("sm",0);
+			resMap.put("rs",new ArrayList());
+		}else{
+			List<MatchRemoteControl>  list = remoteControlResult.getRs();
+			List<TYaokonyunRemoteControl> remoteControlList = new ArrayList<TYaokonyunRemoteControl>();
+			List<QueryRemoteBySrcDTO> dtoList = new ArrayList<QueryRemoteBySrcDTO>();
+			List<QueryRemoteBySrcDTO2> dtoSrcList = new ArrayList<QueryRemoteBySrcDTO2>();
+			for(MatchRemoteControl matchRemoteControl :list){
+				TYaokonyunRemoteControl tYaokonyunRemoteControl = new TYaokonyunRemoteControl(matchRemoteControl);
+				remoteControlList.add(tYaokonyunRemoteControl);
+				QueryRemoteBySrcDTO dto = new QueryRemoteBySrcDTO(matchRemoteControl);
+				QueryRemoteBySrcDTO2 srcDto = new QueryRemoteBySrcDTO2(matchRemoteControl);
+				Integer idx = IndexUtils.getIdx();
+				dto.setIndex(idx);
+				dto.setBrandType(Integer.valueOf(brandId==null?"0":brandId));
+				srcDto.setIndex(idx);
+				srcDto.setBrandType(Integer.valueOf(brandId==null?"0":brandId));
+				dtoList.add(dto);
+				dtoSrcList.add(srcDto);
+			}
+			cmdCache.setIRDeviceInfoList(brandId+"_"+deviceType+"_"+"_remoteControlList",dtoList);
+			cmdCache.setIRDeviceInfoList(brandId+"_"+deviceType+"_"+"_remoteControlListSrc",dtoSrcList);
+			resMap.put("rs",dtoList);
+		}
+		return resMap;
 	}
 
 	// 一键匹配遥控方案——进入空调对码模式
