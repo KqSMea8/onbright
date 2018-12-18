f4b6c1faa9a52c000202ecf26ff59f70da3712e0
Yikuang_Luo <Yikuang_Luo@on-bright.com>
2018-12-18 17:21:21 +0800

    repair device logback

--- springcloud-provider-deivce/src/main/java/com/bright/apollo/DeviceApplication.java
+++ springcloud-provider-deivce/src/main/java/com/bright/apollo/DeviceApplication.java
@@ -28,9 +28,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @MapperScan("com.bright.apollo.dao")
 public class DeviceApplication {
 	//private static final Log logger =LogFactory.getLog(DeviceApplication.class);
-	private static final Logger logger = LoggerFactory.getLogger(DeviceApplication.class);
+//	private static final Logger logger = LoggerFactory.getLogger(DeviceApplication.class);
 	public static void main(String[] args) {
-		logger.info("=====device start====");
+//		logger.info("=====device start====");
+		//System.setProperty("org.apache.commons.logging.LogFactory","org.apache.commons.logging.impl.SLF4JLogFactory");
 		SpringApplication.run(DeviceApplication.class, args);
 	}
 }
--- springcloud-tool/src/main/java/com/bright/apollo/tool/MD5.java
+++ springcloud-tool/src/main/java/com/bright/apollo/tool/MD5.java
@@ -56,7 +56,9 @@ public class MD5 {
 	    return ret.toString();
 	}
 	public static void main(String[] args) throws Exception {
-		String rawPassword="3db9ae5c4cace36f2bdc42940462b7f9";//  12345678luojie
+		String rawPassword="9914223d5f2d787c60cb394e3898f133";//  12345678luojie
+		//ef1531fba98f7f9fd4f1958b3c99e23a
+		//ef1531fba98f7f9fd4f1958b3c99e23a
 		String base64Encrypt = Base64Util.base64Encrypt(rawPassword.toString().getBytes());
  		System.out.println(getMD5Str(base64Encrypt+rawPassword));
 		//System.out.println( (byte) (0xff));
