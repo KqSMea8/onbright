d0a9c8a235b7640add19268418597ea041273095
lyk <Yikuang_Luo@on-bright.com>
2018-12-21 17:58:33 +0800

    web bug

--- springcloud-consumer-feign-config-swagger/src/main/java/com/bright/apollo/config/WebConfig.java
+++ springcloud-consumer-feign-config-swagger/src/main/java/com/bright/apollo/config/WebConfig.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration
+//@Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
     @Override
      public void addResourceHandlers(ResourceHandlerRegistry registry){
-                 registry.addResourceHandler("/**")
-                         .addResourceLocations("classpath:/static/");//这里将/static文件夹定为资源目录，需要根据实际更换
+                 registry.addResourceHandler("/static")
+                         .addResourceLocations("classpath:/resources/static/");//这里将/static文件夹定为资源目录，需要根据实际更换
     }
 }
