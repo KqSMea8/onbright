e99df7dac8834dab8e26cabad96b85504350ebdb
lyk <Yikuang_Luo@on-bright.com>
2018-12-25 14:36:56 +0800

    ir download

--- springcloud-common/src/main/java/com/bright/apollo/common/entity/TYaokonyunKeyCode.java
+++ springcloud-common/src/main/java/com/bright/apollo/common/entity/TYaokonyunKeyCode.java
@@ -1,8 +1,9 @@
 package com.bright.apollo.common.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class TYaokonyunKeyCode {
+public class TYaokonyunKeyCode implements Serializable {
 
     private Integer id;//remark:obox name;length:128; not null,default:null
 
